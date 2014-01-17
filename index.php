9c58d2b9bf005022607960983f960321cc48e124
fpumir <fpumir@terredeweb.fr>
2014-01-17 10:56:18 +0100

    14/01/17
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo 'Hello World !';
+$f3=require('lib/base.php');
+$f3->set('UI','templates/');
+
+$f3->route('GET /',function($f3){
+  echo View::instance()->render('main.html');
+});
+
+$f3->route('GET /users/@alpha',function($f3){
+  echo View::instance()->render('main.html');
+});
+
+
+
+
+
+$f3->run();
 ?>
