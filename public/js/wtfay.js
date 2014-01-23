9d44dead503132b37dfa045e7209c48098098e60
fpumir <fpumir@terredeweb.fr>
2014-01-23 14:37:53 +0100

    full async
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- public/js/wtfay.js
+++ public/js/wtfay.js
@@ -6,4 +6,14 @@ $('.index a').on('click',function(e){
 	.success(function(data){
 		$('.users').html(data);
 	})
+});
+
+$('.users').on('click','a',function(e){
+	e.preventDefault();
+	$.ajax({
+		url:$(this).attr('href')
+	})
+	.success(function(data){
+		$('section + section').html(data);
+	})
 })
