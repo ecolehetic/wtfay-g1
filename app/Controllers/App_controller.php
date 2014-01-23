33b4bcf2c3c052ee3b08abd1f4fc9c200844f087
fpumir <fpumir@terredeweb.fr>
2014-01-23 16:14:55 +0100

    full features MVC
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -2,7 +2,6 @@
 class App_controller{
 
 
-
   function __construct(){
     
   }
@@ -14,7 +13,6 @@ class App_controller{
   function getUsers($f3){
     $model=new App_model();
     $f3->set('users',$model->getUsers($f3,array('alpha'=>$f3->get('PARAMS.alpha'))));
-    
     if($f3->get('AJAX')){
       echo View::instance()->render('partials/users.html');
     }else{
@@ -23,8 +21,8 @@ class App_controller{
   }
   
   function getUser($f3){
-    $user=new DB\SQL\Mapper($f3->get('dB'),'wifiloc');
-    $f3->set('user',$user->load('userId="'.$f3->get('PARAMS.name').'"'));
+    $model=new App_model();
+    $f3->set('user',$model->getUser($f3,array('name'=>$f3->get('PARAMS.name'))));
     if($f3->get('AJAX')){
       echo View::instance()->render('partials/user.html');
     }else{
@@ -32,7 +30,18 @@ class App_controller{
     }
   }
   
+  function searchUsers($f3){
+    $model=new App_model();
+    $f3->set('users',$model->searchUsers($f3,array('keywords'=>$f3->get('POST.name'))));
+    
+    if($f3->get('AJAX')){
+      echo View::instance()->render('partials/users.html');
+    }else{
+      echo View::instance()->render('main.html');
+    }
+  }
   
+
   
 }
 ?>
--- app/Models/App_model.php
+++ app/Models/App_model.php
@@ -9,13 +9,23 @@ class App_model{
 
   }
   
+  function home(){
+    
+  }
+  
   function getUsers($f3,$params){
     $users=new DB\SQL\Mapper($f3->get('dB'),'wifiloc');
     return $users->find('firstname like "'.$params['alpha'].'%"');
   }
   
-  function getUser(){
-    
+  function getUser($f3,$params){
+    $user=new DB\SQL\Mapper($f3->get('dB'),'wifiloc');
+    return $user->load('userId="'.$params['name'].'"');
+  }
+  
+  function searchUsers($f3,$params){
+    $users=new DB\SQL\Mapper($f3->get('dB'),'wifiloc');
+    return $users->find('firstname like "%'.$params['keywords'].'%" or lastname like "%'.$params['keywords'].'%"');
   }
   
 }
