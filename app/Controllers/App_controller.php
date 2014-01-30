08727a518711300b6259ee518417c3225a3cfd6b
fpumir <fpumir@terredeweb.fr>
2014-01-30 10:07:10 +0100

    DRY on controller
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -1,46 +1,43 @@
 <?php
 class App_controller{
 
+var $tpl;
+var $model;
 
   function __construct(){
-    
+    $this->tpl=array('sync'=>'main.html');
+    $model=substr(get_class(),0,strpos(get_class(),'_')+1).'model';
+    if(class_exists($model)){
+      $this->model=new $model();
+    }
   }
   
   function home(){
-    echo View::instance()->render('main.html');
+    
   }
   
   function getUsers($f3){
-    $model=new App_model();
-    $f3->set('users',$model->getUsers($f3,array('alpha'=>$f3->get('PARAMS.alpha'))));
-    if($f3->get('AJAX')){
-      echo View::instance()->render('partials/users.html');
-    }else{
-      echo View::instance()->render('main.html');
-    }
+    $f3->set('users',$this->model->getUsers($f3,array('promo'=>$f3->get('PARAMS.promo'))));
+    $this->tpl['async']='partials/users.html';
   }
   
   function getUser($f3){
-    $model=new App_model();
-    $f3->set('user',$model->getUser($f3,array('name'=>$f3->get('PARAMS.name'))));
-    if($f3->get('AJAX')){
-      echo View::instance()->render('partials/user.html');
-    }else{
-      echo View::instance()->render('main.html');
-    }
+    $f3->set('user',$this->model->getUser($f3,array('name'=>$f3->get('PARAMS.name'))));
+    $this->tpl['async']='partials/user.html';
   }
   
   function searchUsers($f3){
-    $model=new App_model();
-    $f3->set('users',$model->searchUsers($f3,array('keywords'=>$f3->get('POST.name'))));
-    
-    if($f3->get('AJAX')){
-      echo View::instance()->render('partials/users.html');
-    }else{
-      echo View::instance()->render('main.html');
-    }
+    $f3->set('users',$this->model->searchUsers($f3,array('keywords'=>$f3->get('POST.name'))));
+    $this->tpl['async']='partials/users.html';
   }
   
+  function afterroute($f3){
+    if($f3->get('AJAX')){
+      echo View::instance()->render($this->tpl['async']);
+    }else{
+      echo View::instance()->render($this->tpl['sync']);
+    }
+  }
 
   
 }
--- app/Models/App_model.php
+++ app/Models/App_model.php
@@ -15,7 +15,7 @@ class App_model{
   
   function getUsers($f3,$params){
     $users=new DB\SQL\Mapper($f3->get('dB'),'wifiloc');
-    return $users->find('firstname like "'.$params['alpha'].'%"');
+    return $users->find(array('promo=?',$params['promo']),array('order'=>'lastname'));
   }
   
   function getUser($f3,$params){
